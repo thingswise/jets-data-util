058317f53167de899d83ef1c671df0df74f884e9
iqholm <oleg.mokhovikov@iqplanner.com>
2019-03-05 20:50:06 +0300

    clone object impl from server-utils

--- src/index.js
+++ src/index.js
@@ -225,16 +225,11 @@ const Path = require('path');
     return o === void 0;
   }
 
-  function cloneObject(obj) {
-    if (obj === null || typeof obj !== 'object') {
+  function cloneObject(object) {
+    if (!isObject(object)) {
       return obj;
     }
-
-    const temp = obj.constructor(); // give temp the original obj's constructor
-    for (const key in obj) {
-      temp[key] = cloneObject(obj[key]);
-    }
-    return temp;
+    return JSON.parse(JSON.stringify(object));
   }
 
   function isEmpty(value) {
